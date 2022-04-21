afee23a44e5e97c3689bcd85dd8e75fe20920982
Harry Endrulat <harry.endrulat@gmail.com>
2022-04-21 16:55:58 -0500

    nuget package

--- ECommerce/ECommerce/Data/ECommerceContext.cs
+++ ECommerce/ECommerce/Data/ECommerceContext.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace ECommerce
+namespace ECommerce.Data
 {
     public partial class ECommerceContext : DbContext
     {
