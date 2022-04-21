eae407f441fdb5d524caedd7ac6a93eea98dbccb
ele3 <ele3@huskers.unl.edu>
2022-04-20 19:01:15 -0500

    Created new Login Page and updated routing

--- ECommerce/ECommerce/Controllers/HomeController.cs
+++ ECommerce/ECommerce/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace ECommerce.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
