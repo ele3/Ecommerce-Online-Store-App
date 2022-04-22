d52fc4bfd26f64c20e00601420c2182430016ae3
kauman3 <kauman3@huskers.unl.edu>
2022-04-21 20:49:20 -0500

    added connection string

--- ECommerce/ECommerce/Data/ECommerceContext.cs
+++ ECommerce/ECommerce/Data/ECommerceContext.cs
@@ -35,8 +35,12 @@ namespace ECommerce.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql(/*insert connectionString.txt*/, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseMySql(configuration.GetConnectionString("Default"), 
+                                        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
             }
         }
 
