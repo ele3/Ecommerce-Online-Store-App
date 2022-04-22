0917036dbf3443b2a05c60087275ae48d68142f3
ele3 <ele3@huskers.unl.edu>
2022-04-22 10:43:32 -0500

    Updated User Controller to take inputs from the Views, also fixed some errors

--- ECommerce/ECommerce/Controllers/UserController.cs
+++ ECommerce/ECommerce/Controllers/UserController.cs
@@ -4,13 +4,25 @@ namespace ECommerce.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Login()
+        public IActionResult Login(string user, string password)
         {
+            //Code below is printing the inputs into console
+            Console.WriteLine(user);
+            Console.WriteLine(password);
             return View();
         }
 
-        public IActionResult Register()
+        public IActionResult Register(string user, string password, string email, 
+            string country, string state, string street, string zip)
         {
+            // Code below is printing the inputs into console
+            Console.WriteLine(user);
+            Console.WriteLine(password);
+            Console.WriteLine(email);
+            Console.WriteLine(country);
+            Console.WriteLine(state);
+            Console.WriteLine(street);
+            Console.WriteLine(zip);
             return View();
         }
     }
