23d46b9f457c90d0351e81cad6a8309ccc120ebd
Harry Endrulat <harry.endrulat@gmail.com>
2022-04-23 17:20:26 -0500

    this should connect the address to database

--- ECommerce/ECommerce/Models/Address.cs
+++ ECommerce/ECommerce/Models/Address.cs
@@ -10,13 +10,13 @@ namespace ECommerce.Models
         }
 
         public int AddressId { get; set; }
-        public string Zip { get; set; } = null!;
-        public string StreetAddress { get; set; } = null!;
-        public string City { get; set; } = null!;
+        public string Zip { get; set; }
+        public string StreetAddress { get; set; } 
+        public string City { get; set; } 
         public int? StateId { get; set; }
         public int CountryId { get; set; }
 
-        public virtual Country Country { get; set; } = null!;
+        public virtual Country Country { get; set; }
         public virtual State? State { get; set; }
         public virtual ICollection<User> Users { get; set; }
     }
