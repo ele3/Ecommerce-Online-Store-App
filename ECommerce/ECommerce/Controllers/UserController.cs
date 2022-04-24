5822cf7aeeacb1b0c21de5c1d6849af17576ac9a
Harry Endrulat <harry.endrulat@gmail.com>
2022-04-24 18:20:03 -0500

    some basic methods to grab data from the DB and add to the DB

--- ECommerce/ECommerce/Controllers/UserController.cs
+++ ECommerce/ECommerce/Controllers/UserController.cs
@@ -16,10 +16,25 @@ namespace ECommerce.Controllers
             return View();
         }
 
-        [HttpGet]
         public IActionResult Register(string user, string password, string email,
             string country, string state, string street, string zip)
         {
+            //code below takes values in the DB and prints them to the console
+            var mydata = db.States.Select(x => x.State1).ToList();
+            foreach (var VARIABLE in mydata)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            //code below adds states to the DB
+            if (state != null)
+            {
+                State state2 = new State
+                {
+                    State1 = state
+                };
+                db.States.Add(state2);
+                db.SaveChanges();
+            }
             // Code below is printing the inputs into console
             Console.WriteLine(user);
             Console.WriteLine(password);
@@ -28,58 +43,6 @@ namespace ECommerce.Controllers
             Console.WriteLine(state);
             Console.WriteLine(street);
             Console.WriteLine(zip);
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Register(User newUserInfo)
-        {
-            if (ModelState.IsValid)
-            {
-                using (var dbContext = new ECommerceContext())
-                {
-                    State state = new State
-                    {
-                        State1 = newUserInfo.Address.State.State1
-                    };
-                    dbContext.States.Add(state);
-                    dbContext.SaveChanges();
-
-                    var stateq =(from s in dbContext.States where s.State1 == newUserInfo.Address.State.State1
-                        select s.StateId).ToArray();
-
-                    Country country = new Country
-                    {
-                        Country1 = newUserInfo.Address.Country.Country1
-                    };
-                    dbContext.Countries.Add(country);
-                    dbContext.SaveChanges();
-
-                    var countryq = (from c in dbContext.Countries
-                        where c.Country1 == newUserInfo.Address.Country.Country1
-                        select c.CountryId).ToArray();
-
-                    Address address = new Address
-                    {
-                        Zip = newUserInfo.Address.Zip,
-                        StreetAddress = newUserInfo.Address.StreetAddress,
-                        City = newUserInfo.Address.City,
-                        StateId = stateq.ElementAt(0),
-                        CountryId = countryq.ElementAt(0)
-                    };
-                    dbContext.Addresses.Add(address);
-                    dbContext.SaveChanges();
-
-
-                    /*User user = new User //TODO: grab addressId from DB and create User
-                    {
-                        UserName = newUserInfo.UserName,
-
-                    };*/
-                    
-                    //TODO: grab userId from DB and create email
-                }
-            }
             return View();
         }
 
