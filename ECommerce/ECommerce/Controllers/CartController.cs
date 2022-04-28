cb9d5558121da31eb58a0d3dff95d685960136d3
ele3 <ele3@huskers.unl.edu>
2022-04-28 06:51:16 -0500

    Updated CartController to send populated Model to CartView

--- ECommerce/ECommerce/Controllers/CartController.cs
+++ ECommerce/ECommerce/Controllers/CartController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace ECommerce.Controllers
 {
     public class CartController : Controller
     {
+        private ECommerceContext db = new ECommerceContext();
         public IActionResult Cart()
         {
+            User userObject = null;
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                userObject = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("UserSession"));
+            }
+
+            if (userObject != null)
+            {
+                Cart cartObject = db.Carts.Include(x => x.Cartproducts).Where(x => x.UserId == userObject.UserId).SingleOrDefault();
+                foreach (var cartProductObject in cartObject.Cartproducts)
+                {
+                    Product placeholderProduct = db.Products.Find(cartProductObject.ProductId);
+                    cartProductObject.Product = placeholderProduct;
+                }
+
+                return View(cartObject);
+            }
             return View();
         }
     }
