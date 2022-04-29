a25fd68f6681d15d2e5850dd9365345c9cf8975d
ele3 <ele3@huskers.unl.edu>
2022-04-29 17:24:07 -0500

    CheckoutController now passes a Cart Model to View

--- ECommerce/ECommerce/Controllers/CheckoutController.cs
+++ ECommerce/ECommerce/Controllers/CheckoutController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace ECommerce.Controllers
 {
     public class CheckoutController : Controller
     {
+        private ECommerceContext db = new ECommerceContext();
         public IActionResult Checkout(string cardholderName, string cardNumber, string month, string year, string cvv)
         {
-            Console.WriteLine(cardholderName);
-            Console.WriteLine(cardNumber);
-            Console.WriteLine(month);
-            Console.WriteLine(year);
-            Console.WriteLine(cvv);
+            User userObject = null;
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                userObject = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("UserSession"));
+            }
+            if (userObject != null)
+            {
+                Cart cartObject = db.Carts.Include(x => x.Cartproducts).Where(x => x.UserId == userObject.UserId).SingleOrDefault();
+                foreach (var cartProductObject in cartObject.Cartproducts)
+                {
+                    Product placeholderProduct = db.Products.Find(cartProductObject.ProductId);
+                    cartProductObject.Product = placeholderProduct;
+                }
+
+                return View(cartObject);
+            }
             return View();
         }
     }
