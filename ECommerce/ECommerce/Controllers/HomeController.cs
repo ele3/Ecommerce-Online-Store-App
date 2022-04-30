26e4cecd617ab3e7f1d404bc5e9bb073e5fcecdc
ele3 <ele3@huskers.unl.edu>
2022-04-30 18:11:33 -0500

    Updated HomeController and Fixed Form Submission on MainpageView

--- ECommerce/ECommerce/Controllers/HomeController.cs
+++ ECommerce/ECommerce/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using ECommerce.Models;
+using ECommerce.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using ECommerce.Data;
+using Newtonsoft.Json;
 
 namespace ECommerce.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ECommerceContext db = new ECommerceContext();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -23,8 +27,40 @@ namespace ECommerce.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult MainPage()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MainPage(string? selectedProduct)
+        {
+
+            if (selectedProduct != null && HttpContext.Session.GetString("UserSession") != null)
+            {
+                User userObject = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("UserSession"));
+                int selectedProductId = int.Parse(selectedProduct);
+                CatalogController catalogController = new CatalogController();
+                Cart userCart = catalogController.CheckCart(userObject);
+
+                // Increments the Quantity if the Cart Product has been found
+                Cartproduct findCartproduct = catalogController.CheckCartproduct(selectedProductId, userCart);
+                if (findCartproduct != null)
+                {
+                    db.Cartproducts.Find(findCartproduct.CartProductId).Quantity += 1;
+                    db.SaveChanges();
+                    return RedirectToAction("Cart", "Cart");
+                }
+
+                Cartproduct cartProductObject = new Cartproduct();
+                cartProductObject.CartId = userCart.CartId;
+                cartProductObject.ProductId = selectedProductId;
+                cartProductObject.Quantity = 1;
+                db.Cartproducts.Add(cartProductObject);
+                db.SaveChanges();
+                return RedirectToAction("Cart", "Cart");
+            }
             return View();
         }
 
