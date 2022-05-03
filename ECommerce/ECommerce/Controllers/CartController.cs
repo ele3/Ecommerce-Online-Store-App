527dea10e91e75acab0011eb5d3ee85840bcc910
ele3 <ele3@huskers.unl.edu>
2022-05-03 10:29:04 -0500

    Cleaned up code for Checkout and Cart Controllers

--- ECommerce/ECommerce/Controllers/CartController.cs
+++ ECommerce/ECommerce/Controllers/CartController.cs
@@ -11,59 +11,10 @@ namespace ECommerce.Controllers
         private ECommerceContext db = new ECommerceContext();
         public IActionResult Cart()
         {
-            User userObject = null;
-            CartDetail modelObject = new CartDetail();
-            HashSet<Sale> saleList = new HashSet<Sale>();
             if (HttpContext.Session.GetString("UserSession") != null)
             {
-                userObject = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("UserSession"));
-            }
-
-            if (userObject != null)
-            {
-                double? subtotalAmount = 0;
-                double? discountPercentage = 0;
-                double? discountAmount = 0;
-                double? totalAmount = 0;
-                double? tax = 0;
-
-                Cart cartObject = db.Carts.Include(x => x.Cartproducts).Where(x => x.UserId == userObject.UserId).SingleOrDefault();
-                foreach (var cartProductObject in cartObject.Cartproducts)
-                {
-                    Product placeholderProduct = db.Products.Find(cartProductObject.ProductId);
-                    cartProductObject.Product = placeholderProduct;
-                    Sale? saleObject = db.Sales.Where(x => x.CategoryId == placeholderProduct.CategoryId).SingleOrDefault();
-                    subtotalAmount += placeholderProduct.ProductPrice * cartProductObject.Quantity;
-                    if (saleObject != null)
-                    {
-                        saleList.Add(saleObject);
-                    }
-                }
-
-                if (saleList.Count != 0)
-                {
-                    modelObject.Sales = saleList;
-                    foreach (var sale in saleList)
-                    {
-                        discountPercentage += sale.SalePercentDiscount;
-                    }
-
-                    modelObject.DiscountPercentage = discountPercentage;
-                }
-
-                modelObject.Cart = cartObject;
-                discountAmount = Math.Round((double)subtotalAmount * (double)discountPercentage, 2);
-                totalAmount = subtotalAmount - discountAmount;
-                tax = Math.Round((double)totalAmount * 0.05, 2);
-                totalAmount += tax;
-                totalAmount = Math.Round((double) totalAmount, 2);
-                discountPercentage *= 100;
-
-                modelObject.DiscountAmount = discountAmount;
-                modelObject.TotalAmount = totalAmount;
-                modelObject.Tax = tax;
-                modelObject.SubtotalAmount = subtotalAmount;
-                modelObject.DiscountPercentage = discountPercentage;
+                User userObject = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("UserSession"));
+                CartDetail modelObject = getCartDetail(userObject);
                 return View(modelObject);
             }
             return View();
@@ -93,6 +44,57 @@ namespace ECommerce.Controllers
             db.Cartproducts.Remove(cartProductObject);
             await db.SaveChangesAsync();
             return RedirectToAction(nameof(Cart));
+        }
+
+        public CartDetail getCartDetail(User userObject)
+        {
+            CartDetail modelObject = new CartDetail();
+            HashSet<Sale> saleList = new HashSet<Sale>();
+            double? subtotalAmount = 0;
+            double? discountPercentage = 0;
+            double? discountAmount = 0;
+            double? totalAmount = 0;
+            double? tax = 0;
+
+            Cart cartObject = db.Carts.Include(x => x.Cartproducts).Where(x => x.UserId == userObject.UserId).SingleOrDefault();
+            foreach (var cartProductObject in cartObject.Cartproducts)
+            {
+                Product placeholderProduct = db.Products.Find(cartProductObject.ProductId);
+                cartProductObject.Product = placeholderProduct;
+                Sale? saleObject = db.Sales.Where(x => x.CategoryId == placeholderProduct.CategoryId).SingleOrDefault();
+                subtotalAmount += placeholderProduct.ProductPrice * cartProductObject.Quantity;
+                if (saleObject != null)
+                {
+                    saleList.Add(saleObject);
+                }
+            }
+
+            if (saleList.Count != 0)
+            {
+                modelObject.Sales = saleList;
+                foreach (var sale in saleList)
+                {
+                    discountPercentage += sale.SalePercentDiscount;
+                }
+
+                modelObject.DiscountPercentage = discountPercentage;
+            }
+
+            modelObject.Cart = cartObject;
+            discountAmount = Math.Round((double)subtotalAmount * (double)discountPercentage, 2);
+            totalAmount = subtotalAmount - discountAmount;
+            tax = Math.Round((double)totalAmount * 0.05, 2);
+            totalAmount += tax;
+            totalAmount = Math.Round((double)totalAmount, 2);
+            discountPercentage *= 100;
+
+            modelObject.DiscountAmount = discountAmount;
+            modelObject.TotalAmount = totalAmount;
+            modelObject.Tax = tax;
+            modelObject.SubtotalAmount = subtotalAmount;
+            modelObject.DiscountPercentage = discountPercentage;
+
+            return modelObject;
         }
     }
 }
